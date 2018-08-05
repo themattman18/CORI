36b9d686aec66371e2b48365210128ca10327dca
Matt Peterson <themattman18@outlook.com>
2018-08-05 15:15:50 -0500

    - We currently only support Facebook login

--- CORI/Controllers/HomeController.cs
+++ CORI/Controllers/HomeController.cs
@@ -5,9 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CORI.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace CORI.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public IActionResult Index()
