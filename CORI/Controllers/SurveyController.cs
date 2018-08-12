4d47f685d4d64daeeee2bce97fe339d4d7e879a9
Matt Peterson <themattman18@outlook.com>
2018-08-11 20:05:02 -0500

    - First pass at the survey form

---
+++ CORI/Controllers/SurveyController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CORI.Controllers
+{
+    public class SurveyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
