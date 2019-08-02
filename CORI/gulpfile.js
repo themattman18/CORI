1d4d8cc27bc918afb5275aee31cfe34d7acdef4e
Matt Peterson <themattman18@outlook.com>
2019-08-02 10:27:39 -0500

    - Added the font awesome fonts

--- CORI/gulpfile.js
+++ CORI/gulpfile.js
@@ -4,18 +4,27 @@ const paths = {
     nodeModules: './node_modules/',
     scriptsDest: './Scripts/',
     stylesDest: './Content/',
-    frameworksDest: './wwwroot/css/'
+    frameworksDest: './wwwroot/lib/font-awesome/'
 };
 
 
-gulp.task('copy:font-awesome', () => {
+gulp.task('copy:font-awesome:css', () => {
     const cssToCopy = [
         `${paths.nodeModules}font-awesome/css/font-awesome.min.css`
     ];
 
     return gulp.src(cssToCopy)
-        .pipe(gulp.dest(`${paths.frameworksDest}`));
+        .pipe(gulp.dest(`${paths.frameworksDest}css`));
+});
+
+gulp.task('copy:font-awesome:fonts', () => {
+    const cssToCopy = [
+        `${paths.nodeModules}font-awesome/fonts/*`
+    ];
+
+    return gulp.src(cssToCopy)
+        .pipe(gulp.dest(`${paths.frameworksDest}fonts`));
 });
 
 
-gulp.task('default', gulp.series('copy:font-awesome'));
+gulp.task('default', gulp.series('copy:font-awesome:css', 'copy:font-awesome:fonts'));
