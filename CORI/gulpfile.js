956d55186658ad311dfba96d542c7291d1acf33e
Matt Peterson <themattman18@outlook.com>
2019-08-02 10:01:39 -0500

    - Added gulp to project

---
+++ CORI/gulpfile.js
@@ -0,0 +1,21 @@
+﻿var gulp = require('gulp');
+
+const paths = {
+    nodeModules: './node_modules/',
+    scriptsDest: './Scripts/',
+    stylesDest: './Content/',
+    frameworksDest: './wwwroot/css/'
+};
+
+
+gulp.task('copy:font-awesome', () => {
+    const cssToCopy = [
+        `${paths.nodeModules}font-awesome/css/font-awesome.min.css`
+    ];
+
+    return gulp.src(cssToCopy)
+        .pipe(gulp.dest(`${paths.frameworksDest}`));
+});
+
+
+gulp.task('default', gulp.series('copy:font-awesome'));
