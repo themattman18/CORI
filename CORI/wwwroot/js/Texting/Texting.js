8ccf560ad86471c366d3cfd77a8dc8705a0812f8
Matt Peterson <themattman18@outlook.com>
2019-08-18 16:29:09 -0500

    - Added the loading spinner to the texting view

--- CORI/wwwroot/js/Texting/Texting.js
+++ CORI/wwwroot/js/Texting/Texting.js
@@ -3,27 +3,31 @@ var CORI;
     var Texting;
     (function (Texting) {
         function SendText() {
-            var result = confirm("Are you sure you want to send this message?");
-            if (result) {
-                var selectedItems = $("#PhoneNumbers").data("kendoMultiSelect").dataItems();
-                var students = [];
-                for (var i = 0; i < selectedItems.length; i++) {
-                    students.push({
-                        Id: selectedItems[i].id,
-                        FirstName: selectedItems[i].firstName,
-                        LastName: selectedItems[i].lastName,
-                        Phone: selectedItems[i].phone
+            var selectedItems = $("#PhoneNumbers").data("kendoMultiSelect").dataItems();
+            if (selectedItems.length > 0) {
+                var result = confirm("Are you sure you want to send this message to " + selectedItems.length + " students?");
+                if (result) {
+                    LoadingSpinner.Show(true);
+                    var students = [];
+                    for (var i = 0; i < selectedItems.length; i++) {
+                        students.push({
+                            Id: selectedItems[i].id,
+                            FirstName: selectedItems[i].firstName,
+                            LastName: selectedItems[i].lastName,
+                            Phone: selectedItems[i].phone
+                        });
+                    }
+                    var msg = $("#textMessage").val();
+                    var url = $("#textingControls").data("texting-url");
+                    $.ajax({
+                        url: url,
+                        type: "POST",
+                        data: { studentsToText: students, message: msg }
+                    }).done(function (e) {
+                        LoadingSpinner.Show(false);
+                        $("#textMessage").val("");
                     });
                 }
-                var msg = $("#textMessage").val();
-                var url = $("#textingControls").data("texting-url");
-                $.ajax({
-                    url: url,
-                    type: "POST",
-                    data: { studentsToText: students, message: msg }
-                }).done(function (e) {
-                    $("#textMessage").val("");
-                });
             }
         }
         Texting.SendText = SendText;
--- CORI/wwwroot/js/Texting/Texting.ts
+++ CORI/wwwroot/js/Texting/Texting.ts
@@ -1,34 +1,41 @@
 ﻿namespace CORI.Texting {
 
+    declare var LoadingSpinner;
 
     export function SendText() {
+        let selectedItems: any = <kendo.ui.MultiSelect>$("#PhoneNumbers").data("kendoMultiSelect").dataItems();
 
-        var result = confirm("Are you sure you want to send this message?");
+        if (selectedItems.length > 0) {
+            var result = confirm("Are you sure you want to send this message to " + selectedItems.length + " students?");
 
-        if (result) {
-            let selectedItems : any = <kendo.ui.MultiSelect>$("#PhoneNumbers").data("kendoMultiSelect").dataItems();
-            let students = [];
+            if (result) {
 
-            for (var i = 0; i < selectedItems.length; i++) {
-                students.push({
-                    Id : selectedItems[i].id,
-                    FirstName: selectedItems[i].firstName,
-                    LastName: selectedItems[i].lastName,
-                    Phone: selectedItems[i].phone
+                LoadingSpinner.Show(true);
 
+                let students = [];
+
+                for (var i = 0; i < selectedItems.length; i++) {
+                    students.push({
+                        Id: selectedItems[i].id,
+                        FirstName: selectedItems[i].firstName,
+                        LastName: selectedItems[i].lastName,
+                        Phone: selectedItems[i].phone
+
+                    });
+                }
+
+                let msg = $("#textMessage").val() as string;
+                let url = $("#textingControls").data("texting-url") as string;
+
+                $.ajax({
+                    url: url,
+                    type: "POST",
+                    data: { studentsToText: students, message: msg }
+                }).done(function (e) {
+                    LoadingSpinner.Show(false);
+                    $("#textMessage").val("");
                 });
             }
-
-            let msg = $("#textMessage").val() as string;
-            let url = $("#textingControls").data("texting-url") as string;
-
-            $.ajax({
-                url: url,
-                type: "POST",
-                data: { studentsToText: students, message: msg } 
-            }).done(function (e) {
-                $("#textMessage").val("");
-            });
         }
     }
 }
